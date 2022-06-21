fc27a79a1290bafcd2a6b026e4bef4e6a690a1e4
Dilip <77008477+dilipkumar08@users.noreply.github.com>
2022-06-21 10:49:06 +0530

    Create function.r
    checking whether the given number is odd or even by creating a function in R <3

---
+++ function.r
@@ -0,0 +1,12 @@
+oe=function(num)
+{
+ if(num%%2==0)
+ {
+  print("the given number is a even number")
+ }else{
+ print("the given number is a odd number")
+ }
+}
+num=as.numeric(readline(prompt="Enter the number :"))
+oe(num)
+ 
